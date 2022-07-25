be045bdfd4e41a92eed0d7ace58a11243c5b4ae8
emcova6 <109697564+emcova6@users.noreply.github.com>
2022-07-25 16:20:20 -0500

    Initial Commit

---
+++ SignInPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignInPage
+//
+//  Created by Everyone Can Code on 7/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignInPage/SignInPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInPageApp.swift
+//  SignInPage
+//
+//  Created by Everyone Can Code on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SignInPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

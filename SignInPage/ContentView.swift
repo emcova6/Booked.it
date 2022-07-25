dd5a191df3bb4179a4c85b0fdf839a3f269aa3c5
emcova6 <109697564+emcova6@users.noreply.github.com>
2022-07-25 18:27:18 -0500

    Create sign in page
    Coded main sign in page

--- SignInPage/ContentView.swift
+++ SignInPage/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var signIn: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Welcome!")
+                    .font(.title)
+                
+                Image(systemName: "calendar.circle.fill")
+                    .font(.title)
+                    .padding()
+                
+                TextField ("Username", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 150)
+                    .padding()
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 150)
+                    .padding()
+                
+                NavigationLink(destination: Text("Logged in"), isActive: $signIn){ EmptyView() }
+                
+                Button(action: {
+                    signIn = true
+                }, label: {
+                    Text("Sign In")
+                })
+            }
+        }
     }
 }
 

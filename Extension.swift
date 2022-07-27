71a6ad638518f781db0d42d9cffe2da876a6a68b
emcova6 <109697564+emcova6@users.noreply.github.com>
2022-07-27 16:49:45 -0500

    Create extension containing color scheme

---
+++ Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Extension.swift
+//  SignInPage
+//
+//  Created by Everyone Can Code on 7/27/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let alizarin = Color("alizarin")
+    static let amethyst = Color("amethyst")
+    static let asbestos = Color("asbestos")
+    static let belizeHole = Color("belizeHole")
+    static let carrot = Color("carrot")
+    static let clouds = Color("clouds")
+    static let concrete = Color("concrete")
+    static let emerald = Color("emerald")
+    static let greenSea = Color("greenSea")
+    static let midnightBlue = Color("midnightBlue")
+    static let nephritis = Color("nephritis")
+    static let orange = Color("orange")
+    static let peterRiver = Color("peterRiver")
+    static let pomegranate = Color("pomegranate")
+    static let pumpkin = Color("pumpkin")
+    static let silver = Color("silver")
+    static let sunflower = Color("sunflower")
+    static let turquoise = Color("turquoise")
+    static let wetAsphalt = Color("wetAsphalt")
+    static let wisteria = Color("wisteria")
+    //copy+paste above to add more colors
+}
--- SignInPage/ContentView.swift
+++ SignInPage/ContentView.swift
@@ -61,5 +61,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            //.preferredColorScheme(.dark) //changes app to dark mode
     }
 }
